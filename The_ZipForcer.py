69ba9348636f5d3a6374036fec8107d2f6097909
Jaya Prakash Veganti <92370004+Jayking511@users.noreply.github.com>
2022-09-27 16:41:03 +0530

    Update The_ZipForcer.py

--- The_ZipForcer.py
+++ The_ZipForcer.py
@@ -2,64 +2,90 @@ import pyfiglet
 import zipfile
 from pathlib import Path
 import time
-import itertools
+from itertools import permutations
+import threading
+import sys
+import queue
 
 print(pyfiglet.figlet_format("ZipForcer"))
 print("------The ZIP Password BruteForcer------")
 print("[1] Enter")
 print("[0] Exit\n")
-option=input("[?] Enter your choice: ")
+choice=input("[?] Enter your choice: ")
 print()
 
-if option=='0':
-    print("GOOD BYE!")
-    quit()
-elif option=='1':
-    file_path = input("Enter ZIP File PATH: ")
+def checkzipfilepath(file_path):
     if not zipfile.is_zipfile(file_path):
         print("\nzip file path you entered is incorrect.")
         print("Check it and try again.")
-        quit()
-    
-    word_list = input ("Enter Wordlist(.txt File) PATH: ")
-    print()
+        sys.exit()
+    else:
+        pass
+
+def checkwordlist(word_list):
     pth=Path(word_list)
     if not (word_list.endswith(".txt") and pth.exists()) :
         print("word list path you entered is incorrect.")
         print("Check it and try again.")
-        quit()
+        sys.exit()
+    else:
+        pass
+
+def permutation(word_list):
+    wordlist=[]
+    with open(word_list) as file:
+        data=file.read()
+    for i in data.split():
+        wordlist.append(i)
+    finallist=wordlist[:]
+    
+    perm=permutations(wordlist, 2)
+    for i in perm:
+        finallist.append("@".join(i))
+    finalset=set(finallist)
+    return finalset
+
+def password(file_path, word, i, init_time, myqueue):
+    try:
+        with zipfile.ZipFile(file_path) as f:
+            f.extractall(pwd=bytes(word, 'utf-8'))
+        final_time=time.time()
+        tot_time=final_time-init_time
+        print(f"----P4SSW0RD CR4CK3D----\nPassword: {word}\n\nThe time taken to crack the password: {tot_time}\nAttempt no.: {i}\nSpeed: {i/tot_time}passwords/sec\n")
+        myqueue.put("Exit")
+    except Exception as e:
+        pass
+
+
+if choice=='0':
+    print("GOOD BYE!")
+    sys.exit()
+elif choice=='1':
+    file_path = input("Enter ZIP File PATH: ")
+    checkzipfilepath(file_path)
+    
+    word_list = input ("Enter Wordlist(.txt File) PATH: ")
+    print()
+    checkwordlist(word_list)
     
     init_time= time.time()
+    finalset=permutation(word_list)
 
-    with open(word_list) as file:
-        words=file.read()
-    file.close()
-
-    final_list=[]
-    wordlist=words.split()
-    for i in range(1, len(wordlist)+1):
-        perms=itertools.combinations(wordlist, i)
-        for j in list(perms):
-            final_list.append("".join(j))
-    
-    final_set=set(final_list)
-    i=0
-    for word in final_set:
-        i+=1
-        try:
-            with zipfile.ZipFile(file_path) as f:
-                f.extractall(pwd=bytes(word, 'utf-8'))
-            final_time=time.time()
-            tot_time=final_time-init_time
-            print("----P4SSW0RD CR4CK3D----")
-            print("Password: "+word+"\n")
-            print("The time taken to crack the password: "+str(tot_time))
-            print("Attempt no.: "+str(i))
-            print("Speed: "+str(i/tot_time)+" passwords/sec")
-            quit()
-        except Exception as e:
-            pass
+    myqueue=queue.Queue()
+    threads=[]
+    cnt=0
+    for word in finalset:
+        cnt+=1
+        t=threading.Thread(target=password, args=[file_path, word, cnt, init_time, myqueue])
+        t.daemon=True
+        t.start()
+        threads.append(t)
+    for i in threads:
+        i.join()
+    if myqueue.get_nowait()=="Exit":
+        sys.exit()
+    else:
+        pass
     print("Password not found :( ")
-
 else:
     print("Invalid Response !")
